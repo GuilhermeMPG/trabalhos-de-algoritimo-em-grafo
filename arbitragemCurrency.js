fa549332799e3c0b3170b1230861ba42736e0426
Guilherme Gonçalves <guilhermemarcosgoncalves14@gmail.com>
2022-11-17 17:10:23 -0300

    alterações layout

--- arbitragemCurrency.js
+++ arbitragemCurrency.js
@@ -1,105 +1,104 @@
-
-
-
-let grafo= [
-    { arr:[1,2],
-      peso:[49,93.46]
-    },
-    { arr:[0,2],
-        peso:[0.0204,2]
-      },{ arr:[0,1],
-        peso:[0.0107,0.5]
-      }
-]
-buscaProfundidade(grafo,1,2);
-
-
-
-
-
-
-
-
-
+let grafo = [
+     { arr: [1, 2,3], peso: [49, 93.46,5,40], moeda: 'Dolar' },
+     { arr: [0, 2,3], peso: [0.0204, 2,0.066], moeda: 'Rupias Indianas' },
+     { arr: [0, 1,3], peso: [0.0107, 0.5,0.039], moeda: 'Iene' },
+     {arr: [0,1,2],peso:[0.19,15.12,25.86], moeda:'Real'}
+];
+buscaProfundidade(grafo, 3, 3);
 
 function buscaProfundidade(grafo, numeroRaiz, numerosVertice) {
-    let visitado = zerarVisitados(numerosVertice);
-    let pilhadeBusca = [];
-    let verticeBusca;
-    let vizinhosVertice = [];
-    let condicaoDesempilhar = true;
-    let listaVerticesProfundidade = [];
-    let count = 0;
-    let Ciclo = false;
-    let n3;
-    
-    pilhadeBusca.push(numeroRaiz);
-    while (pilhadeBusca.length > 0) {
-        let pesoFinal = 1; 
-         condicaoDesempilhar = true;
+     let visitado = zerarVisitados(numerosVertice);
+     let pilhadeBusca = [];
+     let verticeBusca;
+     let vizinhosVertice = [];
+     let condicaoDesempilhar = true;
+     let listaVerticesProfundidade = [];
+     let count = 0;
+     let Ciclo = false;
+     let n3;
+     let caminhoMoedas=[];
+     
 
-         verticeBusca = pilhadeBusca.at(-1);
-                  
+     pilhadeBusca.push(numeroRaiz);
+     while (pilhadeBusca.length > 0) {
+          let pesoFinal = 1;
+          caminhoMoedas=[];
+          condicaoDesempilhar = true;
 
-         vizinhosVertice = grafo[verticeBusca].arr;
+          verticeBusca = pilhadeBusca.at(-1);
 
-         // listaVerticesProfundidade.push(verticeBusca);
+          vizinhosVertice = grafo[verticeBusca].arr;
 
-         // Busca Ciclo - Inicio
-         for (let i = 0; i < vizinhosVertice.length; i++) {
-              if (pilhadeBusca.length > 1) {
-                   if (vizinhosVertice.includes(numeroRaiz)) { 
-                        if(visitado[verticeBusca] != 1){                 
-                    let pilhaNova = [...pilhadeBusca];   
-                    pilhaNova.push(numeroRaiz);  
-                    
-                    console.log(pilhaNova);
-                    
-                    let qtdMinimadeAresta = pilhaNova.length -1;
-                        for(let i = 0 ; i<qtdMinimadeAresta;i++){
+          // listaVerticesProfundidade.push(verticeBusca);
 
-                            
-                        let indexN= grafo[pilhaNova[i]].arr.indexOf(pilhaNova[i+1])
-                        console.log(indexN)
-                    
-                    pesoFinal=pesoFinal * grafo[pilhaNova[i]].peso[pilhaNova[indexN]];  
-                    console.log(pesoFinal)                      
+          // Busca Ciclo - Inicio
+          for (let i = 0; i < vizinhosVertice.length; i++) {
+               if (pilhadeBusca.length > 1) {
+                    if (vizinhosVertice.includes(numeroRaiz)) {
+                         if (visitado[verticeBusca] != 1) {
+                              let pilhaNova = [...pilhadeBusca];
+                              pilhaNova.push(numeroRaiz);
 
-                       n3 = pesoFinal
-                   }
-                   console.log(n3);
-                }
-                   // Busca Ciclo - Fim
-              }
-            }
-                visitado[verticeBusca] = 1;
-              if (visitado[vizinhosVertice[i]] != 1) {
-                   pilhadeBusca.push(vizinhosVertice[i]);
-                   condicaoDesempilhar = false;
-                   listaVerticesProfundidade.push(
-                        `${verticeBusca} - ${vizinhosVertice[i]}`
-                   );
-                   i = vizinhosVertice.length;
-              }
-         }
-         if (Ciclo === true) {
-              pilhadeBusca.length = 0;
-         }
-         count++;
+                              console.log(pilhaNova);
 
-         if (condicaoDesempilhar == true) {
-              pilhadeBusca.pop();
-         }
-    }
-    let resultado = [listaVerticesProfundidade, visitado, Ciclo];
-    //  console.log(listaVerticesProfundidade);
-    return resultado;
+                              let qtdMinimadeAresta = pilhaNova.length - 1;
+                              for (let i = 0; i < qtdMinimadeAresta; i++) {
+                                   let indexN = grafo[pilhaNova[i]].arr.indexOf(
+                                        pilhaNova[i + 1]
+                                   );
+                                   console.log(indexN + ' index');
+                                   console.log(pilhaNova[i] + ' Pilha');
+                                   pesoFinal =
+                                        pesoFinal *
+                                        grafo[pilhaNova[i]].peso[indexN];
+                                   console.log(
+                                        grafo[pilhaNova[i]].peso[indexN] +
+                                             '<---------'
+                                   );
+                                   console.log(pesoFinal);
+
+                                   n3 = pesoFinal;
+                              }
+                              for(let i =0;i<pilhaNova.length;i++){
+                                let moeda =   grafo[pilhaNova[i]].moeda;
+                                moeda = `${moeda} -->`;
+                                caminhoMoedas.push(moeda);
+                              }
+                              caminhoMoedas= `${caminhoMoedas}`;
+                              console.log(caminhoMoedas.replace(/,/g,' '));
+                              console.log("Valor Final:" + n3);
+                         }
+                         // Busca Ciclo - Fim
+                    }
+               }
+               visitado[verticeBusca] = 1;
+               if (visitado[vizinhosVertice[i]] != 1) {
+                    pilhadeBusca.push(vizinhosVertice[i]);
+                    condicaoDesempilhar = false;
+                    listaVerticesProfundidade.push(
+                         `${verticeBusca} - ${vizinhosVertice[i]}`
+                    );
+                    i = vizinhosVertice.length;
+               }
+          }
+          if (Ciclo === true) {
+               pilhadeBusca.length = 0;
+          }
+          count++;
+
+          if (condicaoDesempilhar == true) {
+               pilhadeBusca.pop();
+          }
+     }
+     let resultado = [listaVerticesProfundidade, visitado, Ciclo];
+     //  console.log(listaVerticesProfundidade);
+     return resultado;
 }
 
 function zerarVisitados(numerosVertice) {
-    let visitado = [];
-    for (let i = 0; i <= numerosVertice; i++) {
-      visitado [i]=0
-    }
-    return visitado;
+     let visitado = [];
+     for (let i = 0; i <= numerosVertice; i++) {
+          visitado[i] = 0;
+     }
+     return visitado;
 }
